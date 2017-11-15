ba548ae742cd9e97bb4edd92cc3ac566a6cabd8a
Maciej Buszka <maciej@buszka.eu>
2017-11-15 17:39:20 +0100

    list 4 ex 2

---
+++ list-4/ex-2/environments.scm
@@ -0,0 +1,35 @@
+(module environments (lib "eopl.ss" "eopl") 
+  
+  ;; builds environment interface, using data structures defined in
+  ;; data-structures.scm. 
+
+  (require "data-structures.scm")
+
+  (provide init-env empty-env extend-env apply-env)
+
+;;;;;;;;;;;;;;;; initial environment ;;;;;;;;;;;;;;;;
+  
+  ;; init-env : () -> Env
+  ;; usage: (init-env) = [i=1, v=5, x=10]
+  ;; (init-env) builds an environment in which i is bound to the
+  ;; expressed value 1, v is bound to the expressed value 5, and x is
+  ;; bound to the expressed value 10.
+  ;; Page: 69
+
+  (define init-env 
+    (lambda ()
+      (extend-env 
+       'i (num-val 1)
+       (extend-env
+        'v (num-val 5)
+        (extend-env
+         'x (num-val 10)
+         (empty-env))))))
+
+;;;;;;;;;;;;;;;; environment constructors and observers ;;;;;;;;;;;;;;;;
+
+  ;; Page: 86
+  (define (apply-env f s) (f s))
+  
+)
+
